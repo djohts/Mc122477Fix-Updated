5729cc6078061ca6eeef23d5e504e63bc49a35ac
djoh <djoh@proxied.host>
2024-11-25 15:01:35 +0200

    Initial commit

---
+++ src/main/java/me/djoh/mc122477fix_updated/GLFWPollCallback.java
@@ -0,0 +1,19 @@
+package me.djoh.mc122477fix_updated;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+
+/**
+ * Callback for polling GLFW key and char events.
+ * Called after each poll.
+ */
+public interface GLFWPollCallback {
+    Event<GLFWPollCallback> EVENT = EventFactory.createArrayBacked(GLFWPollCallback.class,
+            listeners -> () -> {
+                for (GLFWPollCallback listener : listeners) {
+                    listener.onPoll();
+                }
+            });
+
+    void onPoll();
+}
---
+++ src/main/java/me/djoh/mc122477fix_updated/Mc122477Fix.java
@@ -0,0 +1,47 @@
+package me.djoh.mc122477fix_updated;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.ActionResult;
+import org.lwjgl.glfw.GLFW;
+
+@Environment(EnvType.CLIENT)
+public class Mc122477Fix implements ClientModInitializer {
+    private long pollCount;
+    private long prevKeyPoll;
+
+    @Override
+    public void onInitializeClient() {
+        GLFWPollCallback.EVENT.register(() -> this.pollCount++);
+
+        // Key press events are always processed before char type events
+        KeyboardKeyPressedCallback.EVENT.register((window, key, scancode, action, modifiers) -> {
+            MinecraftClient client = MinecraftClient.getInstance();
+            // If this is a key release/repeat OR we're already in a screen (including chat screen), skip
+            if (action != GLFW.GLFW_PRESS || client.currentScreen != null)
+                return ActionResult.PASS;
+
+            // If the chat or command key was pressed, store what poll count it happened on.
+            if (client.options.chatKey.matchesKey(key, scancode) || client.options.commandKey.matchesKey(key, scancode)) {
+                this.prevKeyPoll = this.pollCount;
+            } else {
+                // Otherwise, set to -1
+                this.prevKeyPoll = -1L;
+            }
+
+            return ActionResult.PASS;
+        });
+        KeyboardCharTypedCallback.EVENT.register((window, codepoint, modifiers) -> {
+            // If the previous key poll is -1 or the poll count doesn't match up closely, skip
+            if (this.prevKeyPoll == -1 || this.pollCount - this.prevKeyPoll > 5)
+                return ActionResult.PASS;
+
+            // If we are on a close poll count to when the key press event was polled,
+            // then we should cancel the char type event to ensure it is not passed to the chat field.
+            this.prevKeyPoll = -1;
+            return ActionResult.FAIL;
+        });
+    }
+}
